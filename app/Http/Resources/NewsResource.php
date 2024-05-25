a0d27aa1e0243d97069396b736b3189d4fa52ad6
Stanley Wodson <stanley.wodson@gmail.com>
2024-05-24 23:47:06 -0300

    feat: create resourcer for return data

---
+++ app/Http/Resources/NewsResource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class NewsResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'title' => $this->title,
+            'description' => $this->description,
+            'created_at' => Carbon::parse($this->created_at)->diffForHumans()
+        ];
+    }
+}
